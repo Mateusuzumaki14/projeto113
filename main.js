1508a3c859a51ecd18a1814063334a51e4a12cc0
Mateusuzumaki14 <122612365+Mateusuzumaki14@users.noreply.github.com>
2023-10-31 09:15:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup(){
+    createCanvas(500,500)
+}
+function draw(){
+ background('green')  
+
+    circle(250, 250, 55);
+    ellipse(80, 46, 55, 55);
+    rect(160, 20, 55, 55);
+}
+
